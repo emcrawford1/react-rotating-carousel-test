cafbe7cf6f211e81a424183912b541c0fbe34d1f
mrCrawford <emcrawford1@gmail.com>
2023-01-28 23:18:12 -0600

    Added ProjectInfo component.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,37 @@
 import { getClasses } from "../../helpers/index.js";
+import { ProjectInfo } from "./info.js";
 import { GithubContainer } from "./github.js";
 import { DeployedContainer } from "./deployed.js";
+import { useState } from "react";
 
 export const Card = ({src, imageClass, title, description, githubLink, githubLogo, deployedLink = '', deployedLogo = ''}) => {
   
+  const [isHover, setIsHover] = useState(false);
+  const handleMouseEnter = () => setIsHover(true);
+  const handleMouseExit = () => setIsHover(false);
+
+
   const imageClassNames = getClasses(imageClass);
   
   return (
   <div id='outer-card' class='card'>
     <img id='front-image' src={src} class={imageClassNames} alt='Front card'/>
-    <div id='project-info' class='info'>
-      <div class='project-title'>{title}</div>
-      <div class='project-descr'>{description}</div>
-      <div class='link-container'>
-      <GithubContainer 
-        githubLink={githubLink}
-        githubLogo={githubLogo}
-      />
-      <DeployedContainer 
-        deployedLink={deployedLink}
-        deployedLogo={deployedLogo}
-      />
-      </div>
-
-    </div>
+    <ProjectInfo
+      title={title}
+      description={description}
+      githubContainer={
+        <GithubContainer
+          githubLink={githubLink}
+          githubLogo={githubLogo}
+        />
+      }
+      deployedContainer={
+        <DeployedContainer
+          deployedLink={deployedLink}
+          deployedLogo={deployedLogo}
+        />
+      }
+    />
   </div>
 )}
 
---
+++ src/components/Card/info.js
@@ -0,0 +1,15 @@
+import { info, projectInfo, projectTitle, projectDescr, linkContainer, } from './styling';
+
+export const ProjectInfo = ({ githubLink, gitHubDeployed, title, description }) => {
+  
+  return (
+    <div style={Object.assign({},info, projectInfo)}>
+      <div style={projectTitle}>{title}</div>
+      <div style={projectDescr}>{description}</div>
+      <div style={linkContainer}>
+        <>{githubLink}</>
+        <>{gitHubDeployed}</>
+      </div>
+    </div>
+    )
+}
