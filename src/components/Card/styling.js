78d0aa3b30926494d45e6650ed15fe739340a661
mrCrawford <emcrawford1@gmail.com>
2023-01-29 03:15:17 -0600

    Added styling information in styling.js

--- src/components/Card/styling.js
+++ src/components/Card/styling.js
@@ -1,24 +1,94 @@
-const startingPosition = '100px';
-const rightAdjustment = '200px';
-
-
 const info = {
-  height: '300px',
-  width: '300px',
-  visibility: 'hidden',
-  position: 'absolute',
-  color: 'rgb(122, 32, 65)',
-  backgroundColor: 'black',
-  opacity: '0.8',
-  left: '0',
-  transform: 'scale(1.174)',
-  top: '0',
+  height: "300px",
+  width: "300px",
+  visibility: "hidden",
+  position: "absolute",
+  color: "rgb(122, 32, 65)",
+  backgroundColor: "black",
+  opacity: "0.8",
+  left: "0",
+  transform: "scale(1.174)",
+  top: "0",
+  display: "flex",
+  flexFlow: "column nowrap",
+};
+
+const card = {};
+
+const projectTitle = {
+  fontSize: "21px",
+  marginBotom: "12px",
+  textAlign: "center",
+  fontFamily:
+    "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif",
+};
+
+const projectDescr = {
+  position: "relative",
+  width: "90%",
+  left: "5%",
+  marginBottom: "12px",
+  fontSize: "14px",
+  fontFamily: "Arial, Helvettica, sans-serif",
+};
+
+const githubLink = {
+  textDecoration: "none",
+  color: "#00CCFF",
+  fontSize: "13px",
+};
+
+const deployedTitle = {
+  fontFamily: "Arial, Helvettica, sans-serif",
+  textAlign: "center",
+  fontSize: "11px",
+  paddingTop: "10px",
+  color: "#3399FF",
+};
+
+const githubTitle = {
+  fontFamily: "Arial, Helvettica, sans-serif",
+  textAlign: "center",
+  fontSize: "11px",
+  paddingTop: "10px",
+  color: "#3399FF",
+};
+
+const deployedLink = {
+  textDecoration: "none",
+  color: "#0072de",
+  fontSize: "13px",
+};
+
+const linkContainer = {
   display: 'flex',
-  flexFlow: 'column nowrap',
+  flexDirection: 'row',
+  width: '100%',
+  justifyContent: 'space-evenly',
+  marginTop: 'auto',
+  paddingBottom: '25px',
 }
 
-const card = {
-
+const deployedImg = {
+  width: '88px',
+  height: '90px',
 }
 
-export {info, card}
+const githubImg = {
+  width: '92px',
+  height: '90px',
+}
+
+export {
+  info,
+  card,
+  projectTitle,
+  projectDescr,
+  githubLink,
+  deployedTitle,
+  githubTitle,
+  deployedLink,
+  linkContainer,
+  deployedImg,
+  githubImg,
+};
