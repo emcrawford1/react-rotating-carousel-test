74c75972a44aaac294003db8b872b40064c2c252
mrCrawford <emcrawford1@gmail.com>
2023-01-28 18:40:55 -0600

    Added helper function to convert object with classNames as keys into a string.

---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+const getClasses = classObj => {
+  let classList = '';
+  
+  for(const [key, value] of Object.entries(classObj)) {
+    if(value) classList += key;
+  }
+
+  return classList;
+}
+
+export {getClasses};
