5df0931ab25484ac4aca5a23fe5d767f835f88d5
mrCrawford <emcrawford1@gmail.com>
2023-02-13 00:33:06 -0600

    Added boolean check to see if user wants a card in the carousel.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,12 +5,21 @@ export const InnerContainer = ({ carouselItems }) => {
   return (
     <>
       {carouselItems.map((item, index) => {
+
+        //If the user wishes to add a card to the carousel, the first object in the array should have a key 
+        // named 'card' set to true.  Otherwise the key will not exist or it will be set to false.
+        let cardFlag = carouselItems[0].card ? true : false
+
+        //Skip the first item since it will contain an object with meta data about the carousel items
+        if(index === 0) return <></>;
+
         return (
           <ImageContainer
             index={index}
             src={item.source}
             alt={item.title}
             style={item.style}
+            cardFlag={cardFlag}
           />
         );
       })}
