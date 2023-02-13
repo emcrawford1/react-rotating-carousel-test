1359eee3377ecfe33427930bb7929b6d1bbbfaca
mrCrawford <emcrawford1@gmail.com>
2023-02-13 00:39:47 -0600

    Added comment/note about future change.

--- src/components/Container/ImageContainer.js
+++ src/components/Container/ImageContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card } from "../Card";
 
 
-export const ImageContainer = ({ imgArr, index }) => {
+export const ImageContainer = ({ imgArr, index, cardFlag }) => {
   const img = (
     <img
       id={`image-${index + 1}`}
@@ -14,3 +14,7 @@ export const ImageContainer = ({ imgArr, index }) => {
 
   return <div id={`image-${index + 1}container`}>{img}</div>;
 };
+
+
+//Consider merging the user's styling preferences with embedded styling.  The embedded styling will contain certain defaults.
+//However, these can be overwritten by the user.
