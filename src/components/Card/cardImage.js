4d33542c6cb50965942cb00e93534486bca850b3
mrCrawford <emcrawford1@gmail.com>
2023-02-13 21:37:43 -0600

    Attempting to create more generic image link container to replace the components in deployed.js and github.js

---
+++ src/components/Card/cardImage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  cardImageContainerStyle,
+  cardImageLinkStyle,
+  imageLinkImgStyle,
+  cardImageTitleStyle,
+  cardImageImgStyle,
+  hoverStyle,
+} from "./styling";
+
+export const CardImage = ({
+  imgLink,
+  imgLogo,
+  handleMouseEnter,
+  handleMouseLeave,
+  isHover,
+}) => {
+  return <></>;
+};
