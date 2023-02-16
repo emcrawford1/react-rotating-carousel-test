ba42d02f821294b4fc384bfea1b13d9c9f5c5ec9
mrCrawford <emcrawford1@gmail.com>
2023-02-15 23:29:12 -0600

    Added generic CardImage component to this component.

--- src/components/Card/info.js
+++ src/components/Card/info.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CardImage } from "./cardImage";
 import {
   info,
   projectInfo,
@@ -8,18 +9,32 @@ import {
 } from "./styling";
 
 export const ProjectInfo = ({
-  githubLink,
-  gitHubDeployed,
   title,
   description,
+  images,
+  handleMouseEnter,
+  handleMouseLeave,
+  isHover,
 }) => {
   return (
     <div style={Object.assign({}, info, projectInfo)}>
       <div style={projectTitle}>{title}</div>
       <div style={projectDescr}>{description}</div>
       <div style={linkContainer}>
-        <>{githubLink}</>
-        <>{gitHubDeployed}</>
+      {images.map((image) => {
+        return (
+          <CardImage
+          imgLink={image.imgLink}
+          imgPic={image.imgPic}
+          imgText={image.imgText}
+          userStyleContainer={image.userStyleContainer}
+          userStyleImage={image.userStyleImage}
+          handleMouseEnter={handleMouseEnter}
+          handleMouseLeave={handleMouseLeave}
+          isHover={isHover}
+          />
+          );
+        })}
       </div>
     </div>
   );
