b81f1904f18ef840b00964b825a80cd3c74ceb4e
mrCrawford <emcrawford1@gmail.com>
2023-02-17 00:44:21 -0600

    Added divs for carousel and outer-container.  Also allowed the user to overwrite the default styles of these elements by including the styles in the meta-object passed to the component.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,27 +1,39 @@
 import React from "react";
 import { ImageContainer } from "./ImageContainer";
+import { carousel, carousel_outer_container } from "./styling";
 
 export const InnerContainer = ({ carouselItems }) => {
+
+  // Check if meta-object contains style items.  If so, assign to style variables
+  const carouselStyle = carouselItems[0].carouselStyle
+    ? carouselItems[0].carouselStyle
+    : {};
+  const outerContStyle = carouselItems[0].outerContStyle
+    ? carouselItems[0].outerContStyle
+    : {};
+
   return (
-    <>
-      {
-      carouselItems.map((item, index) => {
+    <div style={Object.assign({}, carousel, carouselStyle)}>
+      <div style={Object.assign({}, carousel_outer_container, outerContStyle)}>
+        {carouselItems.map((item, index) => {
+          // If the user wishes to add a card to the carousel, the first object in the array should have a key
+          // named 'card' set to true.  Otherwise the key will not exist or it will be set to false.
+          let cardFlag = carouselItems[0].card ? true : false;
 
-        // If the user wishes to add a card to the carousel, the first object in the array should have a key 
-        // named 'card' set to true.  Otherwise the key will not exist or it will be set to false.
-        let cardFlag = carouselItems[0].card ? true : false
+          //Skip the first item since it will contain an object with meta data about the carousel items
+          if (index === 0) return <></>;
 
-        //Skip the first item since it will contain an object with meta data about the carousel items
-        if(index === 0) return <></>;
-
-        return (
-          <ImageContainer
-            index={index}
-            item={item}
-            cardFlag={cardFlag}
-          />
-        );
-      })}
-    </>
+          return (
+            <ImageContainer index={index} item={item} cardFlag={cardFlag} />
+          );
+        })}
+      </div>
+    </div>
   );
 };
+
+/*Note - for future reference.  The meta data in carouselItems[0] will need to contain:
+  1) A 'card' flag boolean value, indicating whether or not the user wishes to add a card to the front carousel item. 
+  2) 'Style' variables if the user wishes to overwrite any of the default styling
+  3) TBD
+*/
