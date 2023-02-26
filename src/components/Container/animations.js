c0524011e4cab9d1f64a0ffef480cd94b00c5f03
mrCrawford <emcrawford1@gmail.com>
2023-02-26 17:50:33 -0600

    Added next steps for this componenet in comment.

--- src/components/Container/animations.js
+++ src/components/Container/animations.js
@@ -1,4 +1,13 @@
-// Will need to calculate the rotation of each carousel item based on how many items exist.
+/* Will need to calculate the rotation of each carousel item based on how many items exist.  
+   We will attempt to do this by:
+    1) Passing in the carouselItems array (metadata is in the index 0; data starts in index 1)
+    2) Calculate the number of items in the carouselItems array
+    3) Determine how much the container object will need to rotate to display the next item
+    4) Determine how much each item will need to counter-rotate to continue facing the user
+    5) Add these amounts to each carousel item.
+    6) Return the carouselItems array
+
+*/
 
 export const calculateAnimations = carouselItems => {
 
